b26d43d83f241715e60d332d4fb0459a613d9e81
Abel Balam <abel@etecno.com.mx>
2024-06-15 00:18:36 -0600

    first commit

---
+++ lib/src/shared/classes/image.dart
@@ -0,0 +1,7 @@
+class MyArtistImage {
+  const MyArtistImage({required this.image, required this.sourceName, required this.sourceLink});
+
+  final String image;
+  final String sourceName;
+  final String sourceLink;
+}
